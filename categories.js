326ff64329acb70c93f11fe0f6fc0c684b568576
sofakingbuff <130833612+sofakingbuff@users.noreply.github.com>
2023-04-15 13:02:00 +0300

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,138 @@
+var categories = [  
+	{        
+		title: "Entertainment",   
+	 	links: [     
+			{     
+				link: "https://boards.4channel.org/fit/",          
+				title: "/fit/"        
+			},          
+			{           
+				link: "https://boards.4channel.org/g/",           
+				title: "/g/"        
+			},        
+			{           
+				link: "https://www.radiorivendell.com/",    
+				title: "Rivendell"          
+			},            {         
+				link: "https://rateyourmusic.com/",   
+				title: "rym"            },    
+			{         
+				link: "https://www.imdb.com/", 
+				title: "imdb"            },   
+			{             
+				link: "https://marvelsnapzone.com/collection/",  
+				title: "snap"        
+			}     
+		]
+    },
+    {
+        title: "Resources",
+        links: [
+            {
+                link: "https://jdemeta.net/2019/09/15/exiting-modernity/",
+                title: "exiting modernity"
+            },
+            {
+                link: "https://www.investopedia.com/investing-4427685",
+                title: "investopedia"
+            },
+            {
+                link: "https://www.bogleheads.org/wiki/Bogleheads%C2%AE_investment_philosophy_for_non-US_investors",
+                title: "bogleheads"
+            }
+        ]
+    },
+    {
+        title: "Random",
+        links: [
+            {
+                link: "https://github.com/sizol8/sunset/tree/gh-pages",
+                title: "github"
+            },
+            {
+                link: "https://piratebays.co/",
+                title: "piratebay"
+            },
+            {
+                link: "https://mega.nz/",
+                title: "mega"
+            }
+        ]
+    },
+    {
+        title: "Social Media",
+        links: [
+            {
+                link: "https://twitter.com/",
+                title: "Twitter"
+            },
+            {
+                link: "https://www.instagram.com/",
+                title: "Instagram"
+            },
+            {
+                link: "https://www.reddit.com/",
+                title: "Reddit"
+            },
+            {
+                link: "https://www.tumblr.com/",
+                title: "Tumblr"
+            }
+        ]
+    },
+    {
+        title: "News",
+        links: [
+            {
+                link: "https://www.nytimes.com/",
+                title: "NY Times"
+            },
+            {
+                link: "https://www.cnn.com/",
+                title: "CNN"
+            },
+            {
+                link: "https://www.bbc.com/",
+                title: "BBC"
+            }
+        ]
+    },
+    {
+        title: "Learning",
+        links: [
+            {
+                link: "https://www.khanacademy.org/",
+                title: "Khan Academy"
+            },
+            {
+                link: "https://www.udemy.com/",
+                title: "Udemy"
+            },
+            {
+                link: "https://www.edx.org/",
+                title: "edX"
+            }
+        ]
+    },
+    {
+        title: "Sports",
+        links: [
+            {
+                link: "https://www.espn.com/",
+                title: "ESPN"
+            },
+            {
+                link: "https://www.nba.com/",
+                title: "NBA"
+            },
+            {
+                link: "https://www.nfl.com/",
+                title: "NFL"
+            },
+            {
+                link: "https://www.mlb.com/",
+                title: "MLB"
+            }
+        ]
+	}
+]
